d3badd1e6ab2285188afd29763a94d052129815d
Aniketh <96538528+Anikethpurkayastha@users.noreply.github.com>
2025-06-06 01:20:29 +0530

    Add files via upload

---
+++ task6.py
@@ -0,0 +1,10 @@
+import math
+num = float(input("Enter a number: "))
+square_root = math.sqrt(num)
+natural_log = math.log(num)
+sine_values = math.sin(num)
+
+print("Results:")
+print("Square root:", square_root)
+print("Logarithm:", natural_log)
+print("Sine:", sine_values)
